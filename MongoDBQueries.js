11298c2b5e70cfc0e6f56aee6b5347f80bb48def
Zahiriddin Rustamov <57207346+zahir2000@users.noreply.github.com>
2022-07-04 17:34:25 +0800

    Update MongoDBQueries.js

--- MongoDBQueries.js
+++ MongoDBQueries.js
@@ -12,3 +12,46 @@ db.charts.aggregate([
 { $sort : {"count":-1}},
 { $limit : 10 }
 ]);
+
+.explain("executionStats") to see timing.
+
+
+-- Analysis 2
+
+db.charts.aggregate([
+{ $group : { _id: "$artist", sum:{$sum: "$streams"}}},
+{ $sort : {"sum":-1}},
+{ $limit : 10 }
+]);
+
+
+-- Analysis 3
+
+db.charts.aggregate([
+{ $match: { $and: [ { artist: /.*Sheeran.*/ }, { date: /.*2020.*/  }, { chart: "top200" } ] } },
+{ $group : { _id: "$title", "days":{"$sum": 1},
+"rank": {"$min": "$rank"}}},
+{ $sort : {"rank":1, "days":-1}},
+{ $limit : 10 }
+]);
+
+
+-- Analysis 4
+
+db.charts.aggregate([
+{ $match: { $and: [ { artist: /.*Sheeran.*/ }, { chart: "top200" } ] } },
+{ $group : { _id: "$title", "DaysInCharts":{"$sum": 1},
+"HighestRank": {"$min": "$rank"}, "LowestRank": {"$max": "$rank"}, "AverageRank": {"$avg": "$rank"}}},
+{ $sort : {"AverageRank":1}},
+{ $limit : 10 }
+]);
+
+
+-- Analysis 5
+
+db.charts.aggregate([
+{ $match: { $and: [ { artist: /.*Sheeran.*/ }, { chart: "top200" } ] } },
+{ $group : { _id: "$region", "days":{"$sum": 1}}},
+{ $sort : {"days":-1}},
+{ $limit : 10 }
+]);
